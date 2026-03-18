8f398712ee5d85ba0ee62d6bc7b778c6d0b9ea53
manmen2414 <inotitokokoro@gmail.com>
2026-03-18 18:01:13 +0900

    mmのバグ改善

--- src/ModifyableMover/startup.lua
+++ src/ModifyableMover/startup.lua
@@ -2,6 +2,9 @@ local settingRead = fs.open("setting.json", "r")
 ---@type {from: string,to: string,max_amount: number|nil,filter: ({slot: number}|{id: string}|{tag: string})|nil,atSlot:number|nil}[]
 local setting = textutils.unserialiseJSON(settingRead.readAll());
 settingRead.close();
+if #setting == 0 then
+    os.execute("setting")
+end
 repeat
     for _, Component in ipairs(setting) do
         local Slot = -2;
@@ -9,7 +12,7 @@ repeat
             local from = peripheral.wrap(Component.from);
             local list = from.list()
             if Component.filter.id then
-                for slot, value in ipairs(list) do
+                for slot, value in pairs(list) do
                     if value.name == Component.filter.id then
                         Slot = slot;
                     end
@@ -20,11 +23,11 @@ repeat
                 Slot = -1;
             end
             if Slot == -1 then
-                for slot, _ in ipairs(list) do
-                    from.pushItems(Component.to, slot, Component.max_amount,Component.atSlot)
+                for slot, _ in pairs(list) do
+                    from.pushItems(Component.to, slot, Component.max_amount, Component.atSlot)
                 end
             else
-                from.pushItems(Component.to, Slot, Component.max_amount,Component.atSlot)
+                from.pushItems(Component.to, Slot, Component.max_amount, Component.atSlot)
             end
         end);
         if reason == "Terminated" then
